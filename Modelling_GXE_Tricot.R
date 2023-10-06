71a92d1c62127ca7aa6a60f409144676883369c9
Chinedozi Amaefula <64229182+ca384@users.noreply.github.com>
2023-10-06 16:13:04 -0400

    Numeric data for harvesting and farmers ranking

---
+++ Modelling_GXE_Tricot.R
@@ -0,0 +1,2 @@
+Num_22 <-read_csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/Tricot_22/Tricot_2022_harvest_numeric.csv")
+Rank_22 <- read_csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/Tricot_22/Tricot_2022_ranking.csv")
