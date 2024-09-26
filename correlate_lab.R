2e48a3717656426e06fb843746799b4c918bcd49
Chinedozi Amaefula <64229182+ca384@users.noreply.github.com>
2024-09-26 15:47:55 -0400

    Abbreviated the trait names for the correlation plots

--- correlate_lab.R
+++ correlate_lab.R
@@ -17,7 +17,22 @@ TPA_ov2= TPA_ov%>%
 
 full_chem_blue<-cbind(chem22, TPA_fresh2, TPA_ov2 )
 dim(full_chem_blue)
-
+full_chem_blue=full_chem_blue%>%
+  dplyr::rename("AMY_garri"="amy_garri","CF_garri" = "crude_fiber_garri",
+                 "BD_garri" = "Bulk_Density", "STC_ch" ="Starch_ch",
+                "STC_garri" = "starch_garri", "SC_garri"= "sugar_garri",
+                "SI_garri"="SI" ,  "SP_garri"="SP", "WAC_garri"="WAC",
+                "Adhes_1"= "Adhesiveness_1" , "Cohes_1"="Cohesiveness_1",
+                "Gummi_1"="Gumminess_1" , "Res_1"=  "Resilience_1" ,
+                "Adhes_2"= "Adhesiveness_2", "Cohes_2"="Cohesiveness_2",
+                "Gummi_2"="Gumminess_2" ,   "Resil_2"= "Resilience_2" )
+#%>%
+#   dplyr::select ("X", "varieties", "AMY_ch",
+#                  "BD_garri", "CF_ch",  "DMC", "AMY_garri","CF_garri",
+#                  "SI_garri" ,  "SP_garri",  "STC_ch" , "STC_garri" ,
+#                  "SC_garri" ,  "WAC_garri", "Adhes_1", "Cohes_1",
+#                  "Gummi_1"  ,  "Res_1", "Adhes_2","Cohes_2",
+#                 "Gummi_2"  ,  "Resil_2"  )
 #######
 cor_chem_Blue <- corTest(full_chem_blue[-c(1,2)])
 
@@ -40,7 +55,7 @@ write.csv(x=full_chem_blue, "/Users/ca384/Documents/ChinedoziRepo/TRICOT/output/
 
 All_Chem<-read.csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/output/All_TPA_Chem_BLUE.csv")
 dim(All_Chem)
-    nbAll_eba_worth<- read.csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/output/fres_overnight_eba_worth_estimate.csv")
+   All_eba_worth<- read.csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/output/fres_overnight_eba_worth_estimate.csv")
 dim(All_eba_worth)
 All_eba_worth1<-All_eba_worth%>%
   dplyr::rename("Overall_Eba_2"="Overall.Eba_ov.Quality","Overall_Eba_1"= "Overall.Impression", "Stretchiness_1_R" = "Strechiness_1_R","Stretchiness_2_R"= "Strechiness_2_R",)%>%
@@ -85,3 +100,21 @@ corrplot::corrplot(corr =cor_ranking_chem_eba$r,
 #R
 
 
+cor_ranking_chem_eba2 <- corTest(All_eba_worth1, full_chem_blue[-c(1,2)])
+
+cor_ranking_chem_eba2$r = round(cor_ranking_chem_eba2$r, 2)
+cor_ranking_chem_eba2$p
+cor_ranking_chem_eba2$p.adj
+cor_ranking_chem_eba2$p
+
+
+corrplot::corrplot(corr =cor_ranking_chem_eba2$r,
+                   tl.cex = 0.5,p.mat = cor_ranking_chem_eba2$p,type = "full",
+                   method = 'color',addCoef.col ='black',
+                   sig.level = c(0.001,0.01, 0.05),
+                   insig = "label_sig", pch.cex = 0.7,
+                   number.cex = 0.5,
+                   number.digits = 3, is.corr = T,outline = T, mar = c(0.5,0.5,0.5,0.5))
+#R
+#### New yield correlation with the model that has missing plot as covariate
+onfarm22<- read.csv("/Users/ca384/Documents/ChinedoziRepo/TRICOT/output/2022_blues_model_missing_data.csv")
